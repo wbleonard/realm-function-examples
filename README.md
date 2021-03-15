dc83ca96566cdb342f99fd21279b82370833e80e
Brian Leonard <brianleonard@M-C02DX2QMQ05N.attlocal.net>
2021-03-15 11:16:00 -0400

    Added mirror collection trigger

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-# stitch-function-examples
-A collection of MongoDB Stitch function examples
+# Atlas Trigger & Realm Function Examples
+A collection of MongoDB Atlas Trigger and Realm function examples
+
+## Atlas Triggers
+* [mirror-collection.js](mirror-collection.js) - mirrors changes from the trigger source collection to another collection. 
+
+## Realm Functions
 
 * [find-with-filter.js](find-with-filter.js) - returns an array of documents associated with the supplied filter.
 * [update-many.js](update-many.js) - updating many documents at once.
+
---
+++ mirror-collection.js
@@ -0,0 +1,27 @@
+/* 
+ * This Trigger function demonstrates how to mirror changes from one collection to another. To propertly work:
+ *   Operation Type Insert, Update, Delete and Replace must be selected
+ *   Full Document must be set to ON
+ */
+exports = function(changeEvent) {
+
+    // Get a handle to the mirror collection
+    const mirror_collection = context.services.get("SA-SHARED-DEMO").db("sample_analytics").collection("accounts_mirror");
+    
+    // Access the _id of the changed document:
+    const docId = changeEvent.documentKey._id;
+    
+    // Get the complete updatedated document
+    const fullDocument = changeEvent.fullDocument;
+    
+    if (typeof fullDocument !== 'undefined') {
+      
+      const result = mirror_collection.replaceOne({_id:docId}, fullDocument, {upsert:true});    
+      console.log(result);
+      
+    } else { // The document was deleted
+      
+      const result = mirror_collection.deleteOne({_id:docId});
+      console.log(result);
+    }
+  };
